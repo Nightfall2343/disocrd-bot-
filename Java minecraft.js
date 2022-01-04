401625a5614505b434d544c71c432c0ca0a7aa2b
Nightfall2343 <90920556+Nightfall2343@users.noreply.github.com>
2022-01-04 10:09:27 -0600

    Add files via upload

---
+++ Java minecraft.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Clinet();
+
+
+
+client.once ('ready', () => {
+  consle('bot is Online');
+});
+
+
+const find('Discord.js');
+
+
+
+client.login('put bot toking in here')
